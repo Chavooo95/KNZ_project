92e7c02db0c4580a74c9446eeaf303ddc519d602
Albert Castiñeira Aranda <albertcastiaran@gmail.com>
2024-08-21 17:34:01 +0200

    Add assets + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import tailwindLogo from './assets/tailwindcss.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import MapImage from "./assets/images/initalMap.jpg"
+import Logo from "./assets/images/logoKNZ.png"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div className="flex items-center justify-center pt-20 text-2xl">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://tailwindcss.com/" target="_blank">
-          <img src={tailwindLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-white font-bold text-3xl mt-2">
-        Vite + React + TailwindCSS
-      </h1>
-      <div className="card">
-        <button className="bg-cyan-700 px-4 py-2 rounded-full font-semibold m-4" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="text-md text-gray-400">
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex justify-center">
+        <img src={Logo} className="w-80 h-auto mt-60 hidden" alt="" />
+        <img src={MapImage} className="" alt="" />
       </div>
     </>
   )
