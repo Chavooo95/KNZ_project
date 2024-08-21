31978f9c0f7ffbc431dbe51b158203034d74060e
Albert Castiñeira Aranda <albertcastiaran@gmail.com>
2024-08-21 17:56:29 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import MapImage from "./assets/images/initalMap.jpg"
 import Logo from "./assets/images/logoKNZ.png"
 
 function App() {
@@ -8,8 +7,7 @@ function App() {
   return (
     <>
       <div className="flex justify-center">
-        <img src={Logo} className="w-80 h-auto mt-60 hidden" alt="" />
-        <img src={MapImage} className="" alt="" />
+
       </div>
     </>
   )
