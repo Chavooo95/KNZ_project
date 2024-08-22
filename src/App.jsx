49b9ce4e61037c084e01f747cfe90cb244a27dad
Carlos Terrero <carlosterrero2@gmail.com>
2024-08-22 17:43:46 +0200

    Add entire map view

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
 import Logo from "./assets/images/logoKNZ.png"
+import Map from "./assets/images/H6LOA2E6_output_0.jpeg"
 
 function App() {
 
 
   return (
-    <>
-      <div className="flex justify-center">
-
+    <div className="w-screen h-screen overflow-visible">
+      <div className="flex flex-col justify-center items-center w-full h-full bg-gray-200">
+        <img src={Map} alt="Description" className="object-contain w-full h-full" />
+        <div className="w-screen h-screen">
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
